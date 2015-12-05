1045e7ae5ac072e97c2f18868e57fd35d561cfea
Tina Dominguez <tina@tinadominguez.com>
2015-12-04 19:23:53 -0500

    added more colors to array
    mango is a necessary color.

--- js/color-cycle.js
+++ js/color-cycle.js
@@ -4,7 +4,7 @@
     $.fn.colorCycle = function( options ) {
         
         var settings = $.extend({
-                 colors: ['#549388', '#7A1622', '#D8E7C0', '#427121', '#88B02B', '#80B26B', '#68B3AF', '#F8CC85', '#1A7F87'],
+                 colors: ['#549388', '#7A1622', '#601848', '#FC8D4D','#427121', '#FC694D','#88B02B', '#FABA32','#80B26B', '#68B3AF', '#F8CC85', '#1A7F87', '#F47A63', '#3EC9A7', '#98D9B6', '#0DB2AC', '#C04848', '#F07241','#680148'],
                  animationStartRange: 1000,
                  animationEndRange: 2000,
                  loopStartRange: 1000,
